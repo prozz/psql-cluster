132fd0514d37926c2771111493906723541aede3
prozz <prozz@gmail.com>
2012-03-13 20:51:02 +0100

    improvements

--- start-all.sh
+++ start-all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for file in `find . -type d -name "box*"`; do
-    $file/bin/pg_ctl -D $file/data -l $file/output.log start
+    $file/bin/pg_ctl -D $file/data -l $file/output.log -w start
 done
