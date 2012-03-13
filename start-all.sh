da740518f6ac523c07475f81fce4e048bf084809
prozz <prozz@gmail.com>
2012-03-13 22:15:53 +0100

    mac fix for shared buffers memory

--- start-all.sh
+++ start-all.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# if starting of any node fails on mac, execute following:
+# sudo sysctl -w kern.sysv.shmall=65536
+
 for file in `find . -type d -name "box*"`; do
     $file/bin/pg_ctl -D $file/data -l $file/output.log -w start
 done
